b0146721a5494dc864a43278dad5c4075cbe30d3
DistilledCode <107433905+DistilledCode@users.noreply.github.com>
2024-08-12 09:07:07 +0530

    updated setup.cfg

--- src/dss_selc/utils.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-USE_SOCKS = True
-SOCKS_PROXY = "socks5://127.0.0.1:1080"
-PROXIES = {"http": SOCKS_PROXY, "https": SOCKS_PROXY}
-SRC_PATH = Path(__file__).parent.parent.parent
-DUMP_PATH = SRC_PATH / "dss-selc-dump"
